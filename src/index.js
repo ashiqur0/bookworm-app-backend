8b00d370be9bcdcb52486b1ee28dbc424348a6e8
ashiqur0 <ashiqurrahmans2018@gmail.com>
2026-02-04 07:43:13 +0600

    Set up Express server and authentication routes

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import 'dotenv/config';
+import authRoutes from './routes/authRoutes.js';
+
+const app = express();
+const PORT = process.env.PORT;
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
